cafcdb37791d642b3bf015f1a248bba7b3c97fa7
Saurabh Totey <SaurabhTotey@gmail.com>
2017-01-18 10:26:20 -0700

    Added all basic files and did the homepage

---
+++ Website/ManipulateText.js
@@ -0,0 +1,18 @@
+document.getElementById("siteNav").data-offset-top = document.getElementById("bannerImg").height + "";
+
+function randLetter(){
+  var chars = "abcdefghijklmnopqrstuvwxyz1234567890,./<>?;':'[]{}-=_+\"\\|";
+  return chars[Math.floor((Math.random() * (chars.length + 1))) - 1];
+}
+
+function randBetween(lowerLimit, upperLimit){
+
+}
+
+function encodeMessage(message){
+
+}
+
+function decodeMessage(message){
+
+}
