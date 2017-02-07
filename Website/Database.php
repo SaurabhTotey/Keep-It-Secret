ce618803a3886124322060c20e62c7cdafe84050
Saurabh Totey <SaurabhTotey@gmail.com>
2017-02-07 15:21:25 -0700

    Made Database PHP class

---
+++ Website/Database.php
@@ -0,0 +1,45 @@
+<?php
+  class Database{
+
+    protected static $connection;
+
+    public function connect(){
+      if(!isset(self::$connection)){
+        $config = parse_ini_file('./config.ini');
+        self::$connection = new mysqli('localhost',$config['username'],$config['password'],$config['dbname']);
+      }elseif(self::$connection === false){
+        //TODO
+        return false;
+      }
+      return self::$connection;
+    }
+
+    public function query($query){
+      $connection = $this -> connect();
+      return $connection -> query($query);
+    }
+
+    public function select($query){
+      $rows = array();
+      $result = $this -> query($query);
+      if($result === false){
+        return false;
+      }
+      while ($row = $result -> fetch_assoc()){
+        $rows[] = $row;
+      }
+      return $rows;
+    }
+
+    public function error(){
+      $connection = $this -> connect();
+      return $connection -> error;
+    }
+
+    public function quote($value){
+      $connection = $this -> connect();
+      return "'" . $connection -> real_escape_string($value) . "'";
+    }
+
+  }
+?>
--- Website/ManipulateText.js
+++ Website/ManipulateText.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
   function randLetter(){
-    var chars = "abcdefghijklmnopqrstuvwxyz1234567890,./<>?;':'[]{}-=_+\"\\|";
+    var chars = "abcdefghijklmnopqrstuvwxyz1234567890,./<>?;':'[]{}-=_+\"\\|!@#$%^&*()";
     return chars[Math.floor((Math.random() * (chars.length + 1))) - 1];
   }
 
